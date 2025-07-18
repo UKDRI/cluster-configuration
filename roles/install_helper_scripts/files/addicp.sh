94fb15b40ab00791fbdad345a4da98bb2cc9a24a
Thibaut Hourlier <thibaut.hourlier@ukdri.ac.ac>
2025-07-18 16:09:10 +0100

    Script to copy data to the ADDI workbench

--- roles/install_helper_scripts/files/addicp.sh
+++ roles/install_helper_scripts/files/addicp.sh
@@ -8,7 +8,7 @@ Copy files to ADDI with a MD5 checksum file to verify transfer
   -s  file or directory to copy to ADDI. You must quote the string if using wildcards
   -t  The ADDI token generated from the Upload -> Manage token page
   -r  Recursive flag, mandatory if copying a directory
-  -u  Only upload the files added after TIME, formatted as "2025-05-16T12:00:00Z"
+  -u  Only upload the files added after TIME, formatted as '2025-05-16T12:00:00Z'
   -v  Verbosity flag
   -h  This help message
 
@@ -16,13 +16,13 @@ EOF
 }
 
 verbose=0
-recursive=""
-base_params="--skip-version-check --put-md5"
+recursive=''
+base_params='--skip-version-check --put-md5'
 # The timestamp_filer has been added to facilitate the addition of new singularity
 # images
-timestamp_filter=""
+timestamp_filter=''
 
-while getopts "hvrs:t:u:" option; do
+while getopts 'hvrs:t:u:' option; do
    case $option in
       s)
         source=$OPTARG
@@ -34,7 +34,7 @@ while getopts "hvrs:t:u:" option; do
         timestamp_filter=$OPTARG
         ;;
       r)
-        recursive="--recursive"
+        recursive='--recursive'
         ;;
       v)
         verbose=$((verbose+1))
@@ -47,24 +47,24 @@ while getopts "hvrs:t:u:" option; do
    esac
 done
 
-if [[ -z "${source}" ]] || [[ -z "${token}" ]]; then
+if [[ -z '${source}' ]] || [[ -z '${token}' ]]; then
   help
   exit 2
 fi
 # Create a temporary directory to store the md5 checksum file
 tempdir=$(mktemp -d)
-md5file="${tempdir}/$(basename ${source//\*/_}).md5"
+md5file='${tempdir}/$(basename ${source//\*/_}).md5'
 
-echo "Generating the md5sum file at ${md5file}"
+echo 'Generating the md5sum file at ${md5file}'
 if [[ -d $source ]]; then
-  if [[ -n "${timestamp_filter}" ]]; then
-    base_params="${base_params} --include-after ${timestamp_filter}"
-    for F in $(azcopy copy --dry-run $base_params $recursive $source "${token}" | grep DRYRUN | cut -f3); do
-      echo "${timestamp_filter} $F"
+  if [[ -n '${timestamp_filter}' ]]; then
+    base_params='${base_params} --include-after ${timestamp_filter}'
+    for F in $(azcopy copy --dry-run $base_params $recursive $source '${token}' | grep DRYRUN | cut -f3); do
+      echo '${timestamp_filter} $F'
       md5sum $F >> $md5file
     done
     if [[ ! -s $md5file ]]; then
-      echo "No file found with a timestamp after ${timestamp_filter}"
+      echo 'No file found with a timestamp after ${timestamp_filter}'
       exit 0
     fi
   else
@@ -72,23 +72,23 @@ if [[ -d $source ]]; then
     find $source -type f -exec md5sum {} \; >> $md5file
   fi
   parent=$(dirname $source)
-  if [[ "$parent" != "." ]]; then
-    sed -i "s'$parent/''" $md5file
+  if [[ '$parent' != '.' ]]; then
+    sed -i 's'$parent/''' $md5file
   fi
 else
   md5sum $source > $md5file
-  recursive=""
+  recursive=''
 fi
 
 if [[ $verbose -eq 0 ]]; then
-  base_params="${base_params} --output-level quiet"
+  base_params='${base_params} --output-level quiet'
 elif [[ $verbose -eq 1 ]]; then
-  base_params="${base_params} --output-level essential"
+  base_params='${base_params} --output-level essential'
 fi
 
 # If all azcopy commands are successful, we delete the temp directory
-echo "Copying the files to ADDI"
-azcopy copy $base_params $md5file "${token}" && \
-azcopy copy $base_params $recursive "$source" "${token}" && \
+echo 'Copying the files to ADDI'
+azcopy copy $base_params $md5file '${token}' && \
+azcopy copy $base_params $recursive '$source' '${token}' && \
 rm -rf ${tempdir} && \
-echo "Done!"
+echo 'Done!'
